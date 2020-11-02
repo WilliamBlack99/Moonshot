ca71e56de11ef1c986e877372ba6ded2cc451dd0
WilliamBlack99 <iamwill55@hotmail.com>
2020-11-02 13:51:41 -0800

    renamed image to planet_1 game loop image loading quit button open game window fill background

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello, World")
+if __name__ == "__main__":
+    # initialize modules
+    from os.path import join, dirname, realpath
+    import pygame
+    pygame.init()
+    from pygame.locals import *
+
+    # create game window
+    resolution = (800, 600)
+    window = pygame.display.set_mode(resolution)
+
+    home_dir = dirname(realpath(__file__))
+    planet_1 = pygame.image.load(join(home_dir, "images/planet_1.png"))
+    planet_1.convert()
+
+    # game loop
+    game_running = True
+    while game_running:
+        window.fill((50, 50, 50))
+
+        window.blit(planet_1, (0, 0))
+
+        # reflect changes on the window
+        pygame.display.update()
+
+        # event handling
+        for event in pygame.event.get():
+
+            # quit if x button clicked
+            if event.type == QUIT:
+                game_running = False
+                break
